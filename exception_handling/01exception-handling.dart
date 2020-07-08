30957daa3848aaed5aca736cc373794a3828b726
Vikas Patel <vikaspatelp83@gmail.com>
2020-07-08 19:53:54 +0530

    Exceptions, on, catch, finally

--- exception_handling/01exception-handling.dart
+++ exception_handling/01exception-handling.dart
@@ -11,11 +11,53 @@ void main() {
   // int badDivision = 12 ~/ 0; // EXCEPTION
   //IntegerDivisionByZeroException
 
+  // CASE1: Handle with "on"
+  // use on when we know the exception
   try {
-    int badDivision = 12 ~/ 0;
-    print("Divided by zero = $badDivision");
+    int answer = 12 ~/ 0;
+    print("Division = $answer");
   } on IntegerDivisionByZeroException {
-    // use on whwn we know the exception
     print("Cannot divide by zero");
+  }
+
+  // CASE2: Handle with "catch"
+  // use catch(e) when we don't know the exception
+  try {
+    int answer = 12 ~/ 0;
+    print("Division = $answer");
+  } catch (e) {
+    print("The exception : $e");
+  }
+
+  // CASE3: using STACK TRACE to know the events occured
+  // before exception was thrown
+  try {
+    int answer = 12 ~/ 0;
+    print("Division = $answer");
+  } catch (e, stackTraceObject) {
+    print("The exception : $e");
+    print("Stack Trace:\n$stackTraceObject");
+  }
+
+  // CASE4: finally clause
+  try {
+    int answer = 12 ~/ 0;
+    print("Division = $answer");
+  } catch (e, stackTraceObject) {
+    print("The exception : $e");
+    print("Stack Trace:\n$stackTraceObject");
+  } finally {
+    print("This will always execute");
+  }
+
+  // CASE4: finally clause
+  try {
+    int answer = 12 ~/ 4;
+    print("Division = $answer");
+  } catch (e, stackTraceObject) {
+    print("The exception : $e");
+    print("Stack Trace:\n$stackTraceObject");
+  } finally {
+    print("This will always execute");
   }
 }
