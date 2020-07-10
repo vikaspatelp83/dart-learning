4df91959a73164e465d214cfc2ec2cfcfa46b85d
Vikas Patel <vikaspatelp83@gmail.com>
2020-07-11 01:42:01 +0530

    Parse int, double, bool from String

--- 08 io/01stdin.dart
+++ 08 io/01stdin.dart
@@ -1,6 +1,26 @@
 import "dart:io";
 
 void main() {
+  // String
+  print("Name? ");
   String name = stdin.readLineSync();
-  print("Hello, $name");
+  // print("Hello, $name");
+
+  // int
+  print("Age? ");
+  int age = int.parse(stdin.readLineSync());
+
+  // double
+  print("Rating Percentage? ");
+  double perc = double.parse(stdin.readLineSync());
+
+  // bool
+  print("Married?(true/false) ");
+  bool md = bool.fromEnvironment(stdin.readLineSync());
+
+  print("Name: $name\nAge : $age\nPercentage : $perc\nMarried : $md");
+
+  if (md) {
+    print(!md);
+  }
 }
